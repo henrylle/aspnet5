f3b7ba2b2135a8753728af1e3068c3e99709a635
Henrylle Maia <henryllemaia@gmail.com>
2015-09-30 00:51:44 -0300

    sample 3

--- WebApplication3/src/WebApplication3/wwwroot/_references.js
+++ WebApplication3/src/WebApplication3/wwwroot/_references.js
@@ -1,4 +1,3 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="../deploy/approot/src/WebApplication3/gulpfile.js" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/site.js" />
